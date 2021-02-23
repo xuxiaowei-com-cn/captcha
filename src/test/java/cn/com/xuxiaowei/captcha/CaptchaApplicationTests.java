756b6bfdd0617d2aa9d98195214602a1d643357a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-23 17:46:44 +0800

    Initial commit

---
+++ src/test/java/cn/com/xuxiaowei/captcha/CaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.captcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
